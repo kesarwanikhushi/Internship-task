7060b4687b3f98270666270e9945623941986ce5
kesarwanikhushi <kesarwani.khushi121@gmail.com>
2026-01-14 21:42:44 +0530

    Add Vercel serverless wrapper and serverless-http dependency

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,3 +3,8 @@ const app = require('../server');
 
 module.exports = app;
 module.exports.handler = serverless(app);
+const serverless = require('serverless-http');
+const app = require('../server');
+
+module.exports = app;
+module.exports.handler = serverless(app);
